3ff1b6fe9ce2a9c95b63a8d09ed5f3b53a19198f
Luisrasconn <L24550277@chihuahua2.tecnm.mx>
2024-08-28 21:42:14 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVIL/src/eva1_3_automovil/EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_automovil;
+
+/**
+ *
+ * @author user
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String modelo;
+        String marca;
+        double precio;
+        int year;
+        
+        marca= "ford";
+        modelo="mustang";
+        precio=1000000.4;
+        year=2022;
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(precio);
+        System.out.println(year);
+        
+          
+    }
+    
+}

fc8129fb3c27dabbb7311db9fa82a4ea3dab867f
Luisrasconn <L24550277@chihuahua2.tecnm.mx>
2024-08-28 21:05:22 -0600

    Add files via upload

---
+++ EVA_1_2-VARIABLES-main/EVA1_2_VARIABLES/src/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_2_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                                           //TIPOS DE DATOS
+        System.out.println("HOLA MUNDO");  //cadena de texto
+        System.out.println(100);           // un numero entero
+        System.out.println('c');           // un caracter
+        System.out.println(true);          // valores logicos
+        System.out.println(false);
+        System.out.println(1000.52);       // numeros con fraccion (con punto flotante) 
+        
+        //TIPOS DE DATOS
+        //ENTEROS
+        //int
+        //long
+        //byte
+        //short
+        
+        //PUNTO FLOTANTE
+        //double
+        // float
+        
+        // TEXTO
+        //String
+        
+        //CARACTER
+        //char
+        //LOGICOS (true or false)
+        //boolean
+        
+        //creacion y uso de variables
+        //DECLARIACION
+        //tipo_de_dato nombre de la variable; que operaciones debe o puede hacer.-cuanta memorria va a utilizar
+        //ASIGNACION
+        int edad;//declaro una variable de tupo entero llamda edad
+        double edad2;// no podemos ntener una variable con el mismo nombre
+        String nombre;
+        String apellido;
+        double promedio;
+        //poner valor a la variable
+        edad=18;// estoy asignando el valor 18 ndentro de la variable edad
+        
+        System.out.println(edad);//println tambien imprime variable
+        
+        
+    }
+    
+}
